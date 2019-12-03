a0ed03ec53176b87d4d57489ec558fb0a196a031
KatiusciaNovaesdeSa <54957332+KatiusciaNovaesdeSa@users.noreply.github.com>
2019-12-03 14:21:15 -0500

    Update server.js

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,5 +1,14 @@
-const express = require('express');
+const express = require('express'),
+      bodyParser = require('body-parser')
+
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/src/'));
+//app.use(bodyParser.json());
+const port = process.env.PORT || 4200;
+app.listen(port, function(){
+    console.log('Listening on port ' + port);
+});
+    
+
+      
