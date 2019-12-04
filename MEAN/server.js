c74b6593a20a9668d5ee2307d052c8da6621878f
Raphael Strada <raphael.strada@gmail.com>
2019-12-04 16:41:31 -0500

    Fixing server mongo

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,6 +1,28 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       path = require('path');
+      cors = require('cors'),
+      mongoose = require('mongoose'),
+      config = require('backend/config/DB');
+      playerRoutes = require('backend/routes/playerRoutes');
+      gameRoutes=require('backend/routes/gamesRoutes')
+      
+      mongoose.Promise = global.Promise;
+      mongoose.connect(config.DB).then(
+          () => {console.log('Database connected') },
+          err => { console.log('Can not connect to the database'+ err)}
+        );
+
+const appBackend = express();
+appBackend.use(bodyParser.json());
+appBackend.use(cors());
+const portBackend = 4000;
+appBackend.use('/players', playerRoutes);
+appBackend.use('/games', gameRoutes);
+appBackend.listen(portBackend, function(){
+    console.log('Listening on port ' + portBackend);
+});
+       
 
 const app = express();
 
