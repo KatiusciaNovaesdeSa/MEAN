7d63b992a9cdc844d10959c01f9ef3395cbea57f
Raphael Strada <raphael.strada@gmail.com>
2019-12-04 15:28:15 -0500

    Fixing routes

--- MEAN/server.js
+++ MEAN/server.js
@@ -1,10 +1,22 @@
 const express = require('express'),
-      bodyParser = require('body-parser')
+      bodyParser = require('body-parser'),
+      path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/testApp'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(express.static(path.join(__dirname, 'dist/testApp')));
+
+///app.use(express.static(__dirname + '/dist/testApp'));
 //app.use(bodyParser.json());
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/testApp/index.html'));
+});
+
+
+
 const port = process.env.PORT || 4200;
 app.listen(port, function(){
     console.log('Listening on port ' + port);
